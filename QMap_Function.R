011ab69537fbc9234906a3d34efdf28e81a9a3ba
Juan Carlos Rubio-Polania <71674994+Keyanswers@users.noreply.github.com>
2022-05-14 01:32:35 -0500

    Update QMap_Function.R

--- QMap_Function.R
+++ QMap_Function.R
@@ -1,5 +1,4 @@
-
-QMap = function (df,v.names,n,sunits,skl,ftext,col1,xlab,ylab,pox,poy){
+QMap = function (df,vnames,n,sunits,ftext,col1,xlab,ylab,sta, pox, poy){
   
   pck =c('maps','mapplots','oceanmap','extrafont','ggplot2','marmap')
   if(length(pck[!pck %in% rownames(data.frame(installed.packages()))])>0){
@@ -9,53 +8,76 @@ QMap = function (df,v.names,n,sunits,skl,ftext,col1,xlab,ylab,pox,poy){
     lapply(pck[1:5],require,character.only=TRUE)
   }
   
-  if (dim(df)[1]>2){
-    Lon1 = min(df[,1]); Lon2 = max(df[,1])
-    Lat1 = min(df[,2]); Lat2 = max(df[,2]) 
-    
-    skl = ifelse(missing(skl),1,skl)
-    v.names = ifelse(missing(v.names),colnames(df)[n],v.names)
-    n = ifelse(missing(n),NULL,n)
-    
-    M = c()
-    for (i in seq_along(colnames(df))){
-      if(n==0){
-        M = rep(1,(dim(df)[1]))
-      } else if (is.null(n)) {
-      M = rep(0,(dim(df)[1]))
+  Lon1 = min(df[,1]); Lon2 = max(df[,1])
+  Lat1 = min(df[,2]); Lat2 = max(df[,2])
+  
+  Bath = marmap::getNOAA.bathy(Lon1,Lon2,Lat1,Lat2, resolution = 1, keep = TRUE)
+  
+  Df = na.omit(df)
+  n = ifelse(missing(n),0,n)
+  vnames = ifelse(missing(vnames),colnames(Df)[n],vnames)
+  
+  if (missing(sta)){
+    sta = rownames(Df)
+  } else {
+    sta = as.character(Df[,sta])
+    rownames(Df) = sta
+  }
+  
+  M = c()
+  for (i in seq_along(colnames(Df))){
+    if(n==0){
+      M = rep(2.5,(dim(Df)[1]))
     } else{
-      M = as.numeric(df[,n]/skl)
+      M = as.numeric(Df[,n])
     }
   }
-  Bath = marmap::getNOAA.bathy(Lon1,Lon2,Lat1,Lat2, resolution = 1, keep = TRUE)
-  autoplot(Bath,geom = c("r","c")) + marmap::scale_fill_etopo() +
-    geom_point(aes(x = df[,1], y = df[,2], size = M),data = df, colour = col1) +
-    theme(text=element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 14, color = "black"),
-          axis.text = element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 14, color = "black"))+
-    labs(x=ifelse(missing(xlab),"Longitude",xlab),
-         y=ifelse(missing(ylab),"Latitude",ylab),
-         fill = ifelse(missing(sunits),"Depth (m)",sunits),
-         size = v.names) +
-    geom_text(data = df, mapping = aes(x = df[,1], y = df[,2], label = rownames(df)),
-              family = ifelse(missing(ftext),"Times New Roman",ftext), size = 3,
-              position = position_nudge(x = ifelse(missing(pox),0,pox), y = ifelse(missing(poy),0,poy))) +
-    theme(legend.title.align = 0.5)
   
-} else {
-  
-  Min = signif(sapply(df,min),digits = 4)
-  Max = signif(sapply(df,max),digits = 4)
-  Bath = marmap::getNOAA.bathy(Min[1],Max[1],Min[2],Max[2], resolution = 1, keep = TRUE)
-  autoplot(Bath,geom = c("r","c")) + 
-    marmap::scale_fill_etopo() +
-    labs(x = ifelse(missing(xlab),"Longitude",xlab),
-         y = ifelse(missing(ylab),"Latitude",ylab)) + 
-    theme(text = element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 14, color = "black"),
-          axis.text = element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 14, color = "black")) +
-    theme(legend.position = "none")
- }
+  if (dim(Df)[1]>2 & mean(M,na.rm=TRUE)==2.5){
+    
+    autoplot(Bath,geom = c("r","c")) + marmap::scale_fill_etopo() +
+      geom_point(aes(x = Df[,1], y = Df[,2], size = factor(M, labels=" ")),data = Df, colour = ifelse(missing(col1),"black",col1)) +
+      theme(text=element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 16, color = "black"),
+            axis.text = element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 16, color = "black")) +
+      labs(x=ifelse(missing(xlab),"Longitude",xlab),
+           y=ifelse(missing(ylab),"Latitude",ylab),
+           fill = ifelse(missing(sunits),"Meters (m)",sunits),
+           size = ifelse(is.na(vnames),"Stations",vnames))+
+      geom_text(data = Df, mapping = aes(x = Df[,1], y = Df[,2], label = sta),
+                family = ifelse(missing(ftext),"Times New Roman",ftext), size = 4,
+                position = position_nudge(x = ifelse(missing(pox),0,pox), y = ifelse(missing(poy),-0.09,poy))) +
+      theme(legend.title.align = 0.5)
+    
+  } else if (dim(Df)[1]>2 & mean(M,na.rm=TRUE)!=2.5) {
+    
+    autoplot(Bath,geom = c("r","c")) + marmap::scale_fill_etopo() +
+      geom_point(aes(x = Df[,1], y = Df[,2], size = M),data = Df, colour = ifelse(missing(col1),"black",col1)) +
+      theme(text=element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 16, color = "black"),
+            axis.text = element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 16, color = "black"))+
+      labs(x=ifelse(missing(xlab),"Longitude",xlab),
+           y=ifelse(missing(ylab),"Latitude",ylab),
+           fill = ifelse(missing(sunits),"Meters (m)",sunits),
+           size = vnames) +
+      geom_text(data = Df, mapping = aes(x = Df[,1], y = Df[,2], label = sta),
+                family = ifelse(missing(ftext),"Times New Roman",ftext), size = 4,
+                position = position_nudge(x = ifelse(missing(pox),0,pox), y = ifelse(missing(poy),0,poy))) +
+      theme(legend.title.align = 0.5)
+    
+  } else {
+    
+    Min = signif(sapply(Df,min),digits = 4)
+    Max = signif(sapply(Df,max),digits = 4)
+    Bath = marmap::getNOAA.bathy(Min[1],Max[1],Min[2],Max[2], resolution = 1, keep = TRUE)
+    autoplot(Bath,geom = c("r","c")) + 
+      marmap::scale_fill_etopo() + 
+      labs(x = ifelse(missing(xlab),"Longitude",xlab),
+           y = ifelse(missing(ylab),"Latitude",ylab),
+           fill = "Meters (m)") + 
+      theme(text = element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 14, color = "black"),
+            axis.text = element_text(family = ifelse(missing(ftext),"Times New Roman",ftext), size = 14, color = "black")) +
+      theme(legend.title.align = 0.5)
+  }
 }
 
-
-save(QMap, file = "QuickMap.Rdata")
+save(QMap, file="QuickMap.Rdata")
 load("QuickMap.Rdata")
